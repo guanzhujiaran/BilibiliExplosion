fd14f6eb02d0a80671877d90b91bd25486dc61b5
星瞳 <1944637830@qq.com>
2025-10-02 14:29:43 +0800

    1

--- FastapiApp/Service/GetOthersLotDyn/get_other_lot_main.py
+++ FastapiApp/Service/GetOthersLotDyn/get_other_lot_main.py
@@ -1623,7 +1623,8 @@ class GetOthersLotDynRobot:
                                 565064296,
                                 693445761,
                                 7538278,
-                                3546876406139528
+                                3546876406139528,
+                                482187762
                                 ]
                 self.bili_space_user_items_set.update(
                     [BiliSpaceUserItem(uid=x, lot_round_id=self.nowRound.lotRound_id) for x in default_list])
